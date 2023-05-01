035e2c21d538d6bd5a80d2832f4cf495b6808177
rolandobarros27 <123201689+rolandobarros27@users.noreply.github.com>
2023-05-01 08:41:56 -0400

    Completing the scripts

--- script.sql
+++ script.sql
@@ -26,3 +26,50 @@ TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.subject
     OWNER to postgres;
+
+-- Table: public.city
+
+-- DROP TABLE IF EXISTS public.city;
+
+CREATE TABLE IF NOT EXISTS public.city
+(
+    id bigint NOT NULL DEFAULT nextval('"City_id_seq"'::regclass),
+    state character varying COLLATE pg_catalog."default",
+    city character varying COLLATE pg_catalog."default",
+    CONSTRAINT "City_pkey" PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.city
+    OWNER to postgres;
+    
+    -- SEQUENCE: public.subject_id_seq
+
+-- DROP SEQUENCE IF EXISTS public.subject_id_seq;
+
+CREATE SEQUENCE IF NOT EXISTS public.subject_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1
+    OWNED BY subject.id;
+
+ALTER SEQUENCE public.subject_id_seq
+    OWNER TO postgres;
+    
+    -- SEQUENCE: public.City_id_seq
+
+-- DROP SEQUENCE IF EXISTS public."City_id_seq";
+
+CREATE SEQUENCE IF NOT EXISTS public."City_id_seq"
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1
+    OWNED BY city.id;
+
+ALTER SEQUENCE public."City_id_seq"
+    OWNER TO postgres;
